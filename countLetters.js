94c747c5441d983c2954cf9cf99f019c80fd3b75
arnoldthchan <arnoldthchan@gmail.com>
2017-06-28 13:12:34 +0000

    countLetters.js working

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,27 @@
-function countLetters(str){
-  console.log(str);
+//Returns all the unique characters that exist in a string
+//Function should also report back how many instances of
+//each letter found in the string
 
+var counted = {};
+
+function countLetters(word){
+  justWord = (word.split(' ').join(''));
+  for (var i = 0; i < justWord.length; i++){
+    var character = justWord.charAt(i);
+    if (counted[character]){
+      // Adds 1 to character already within object
+      // console.log('Letter Added:', justWord[i]);
+      counted[character] ++;
+    } else {
+      // Adds new letter to object
+      // console.log('New Letter Added:', justWord[i]);
+      counted[character] = 1;
+    }
+  }
+  return counted;
 
 }
 
-countLetters('asdasd');
+countLetters('lighthouse in the house');
+countLetters('aaaaassssdddffg');
+console.log(counted);
